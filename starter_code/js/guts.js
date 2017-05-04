2bd5381f105dcc43f7a24f32a1cbd1b1b862632b
eeerrrf <wahum@s.sfusd.edu>
2017-05-04 16:49:22 -0700

    Update guts.js

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -3,35 +3,93 @@
     // multiply, divide, subtract, add, power, square_root, logBase10, generateRandomNumber
 
 
+// 1. divide
+//    write a function named "divide" that takes in two parameters and returns their quotient
 function divide(x,y) {
-	//Write your code below this line
-
+var a=(x/5);
+return a;
 }
 
-function multiply(x,y) {
+console.log(divide(5));
+// 2. multiply
+// write a function named
+// "multiply" that takes in two
+// parameters and returns
+// their product
+function multiply(m,u){
+    var b=(m*10);
+    return b;
+    }
 
-}
+console.log(multiply(5));
+// 3. subtract 
+//    write a function named
+//    "subtract" that takes in two
+// parameters and returns
+// their difference
 
-function subtract(x,y) {
+function subtract(d,e){
+    var c=(d-10);
+    return c;
+    }
 
-}
+console.log(subtract(5));
 
-function add(x,y) {
+// 4. add
+//    write a function named "add" that takes in two parameters and returns their sum
 
-}
+function add(d,e){
+    var f=(d+10);
+    return f;
+    }
 
-function power(x,y) {
+console.log(add(5));
 
-}
 
-function square_root(x) {
+// 5. power
+//    write a function named "power" that takes in two parameters and returns their result
 
-}
+function power(g,h){
+    var i=Math.pow (g,10);
+    return i;
+    }
 
-function logBase10(x) {
+console.log(power(5));
 
-}
+// 6. square_root
+//    write a function named "square_root" that takes in one parameter and returns their result
 
-function generateRandomNumber() {
+function square_root(j,k){
+    var l=Math.sqrt(j,10);
+    return l;
+    }
 
-}
+console.log(square_root(100));
+
+
+// 7. logBase10
+//    write a function named "logBase10" that takes in one parameter and returns their result
+
+function logBase10(m,n){
+    var o=Math.log10(m,10);
+    return o;
+    }
+
+console.log(logBase10(10));
+
+// 8. generateRandomNumber
+//    write a function named "generateRandomNumber" that takes in no parameters and returns a random number
+
+function generateRandomnumber(){
+    return Math.random();
+    }
+
+console.log(generateRandomnumber(10));
+
+console.assert(add(7));
+
+// BONUS: write assertions to test whether your functions work!
+// Here are some examples for divide(x,y), can you write more for the other functions?
+//
+// console.assert(divide(1,1) === 1, "1/1 should equal 1");
+// console.assert(divide(1,2) === 0.5, "1/2 should equal 0.5");
