4222d11184813332d6f1772ea1308807d2a4eb0f
eeerrrf <wahum@s.sfusd.edu>
2017-05-04 16:55:50 -0700

    Update guts.js

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -6,22 +6,22 @@
 // 1. divide
 //    write a function named "divide" that takes in two parameters and returns their quotient
 function divide(x,y) {
-var a=(x/5);
+var a=(x/y);
 return a;
 }
 
-console.log(divide(5));
+console.log(divide(5,5));
 // 2. multiply
 // write a function named
 // "multiply" that takes in two
 // parameters and returns
 // their product
 function multiply(m,u){
-    var b=(m*10);
+    var b=(m*u);
     return b;
     }
 
-console.log(multiply(5));
+console.log(multiply(5,5));
 // 3. subtract 
 //    write a function named
 //    "subtract" that takes in two
@@ -29,53 +29,53 @@ console.log(multiply(5));
 // their difference
 
 function subtract(d,e){
-    var c=(d-10);
+    var c=(d-e);
     return c;
     }
 
-console.log(subtract(5));
+console.log(subtract(5,5));
 
 // 4. add
 //    write a function named "add" that takes in two parameters and returns their sum
 
 function add(d,e){
-    var f=(d+10);
+    var f=(d+e);
     return f;
     }
 
-console.log(add(5));
+console.log(add(5,5));
 
 
 // 5. power
 //    write a function named "power" that takes in two parameters and returns their result
 
 function power(g,h){
-    var i=Math.pow (g,10);
+    var i=Math.pow (g,h);
     return i;
     }
 
-console.log(power(5));
+console.log(power(5,5));
 
 // 6. square_root
 //    write a function named "square_root" that takes in one parameter and returns their result
 
 function square_root(j,k){
-    var l=Math.sqrt(j,10);
+    var l=Math.sqrt(j,k);
     return l;
     }
 
-console.log(square_root(100));
+console.log(square_root(100,90));
 
 
 // 7. logBase10
 //    write a function named "logBase10" that takes in one parameter and returns their result
 
 function logBase10(m,n){
-    var o=Math.log10(m,10);
+    var o=Math.log10(m,n);
     return o;
     }
 
-console.log(logBase10(10));
+console.log(logBase10(10,10));
 
 // 8. generateRandomNumber
 //    write a function named "generateRandomNumber" that takes in no parameters and returns a random number
